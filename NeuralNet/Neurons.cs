06d53a430cfd519e81fd5f9f433754870b28480e
Keep3r <keep3r.mail@keemail.me>
2018-05-04 17:05:07 -0300

    Initial commit

---
+++ NeuralNet/Neurons.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace NeuralNet
+{
+    class Neurons
+    {
+        double[] Neuron { get; set; }
+        double[] Weight { get; set; }
+        double DesiredOutput = 1.0;
+        double LearningRate = 0.1;
+
+        public void SetNeurons(double[] neurons)
+        {
+            Neuron = neurons;
+        }
+
+        public void SetWeights(double[] weights)
+        {
+            Weight = weights;
+        }
+
+        public double EvaluateNeuralNet(double[] neuron, double[] weight)
+        {
+            double result = 0;
+
+            for(int i = 0; i < neuron.Length; i++)
+            {
+                double layerValue = neuron[i] * weight[i];
+
+                result += layerValue;
+            }
+
+            return result;
+        }
+
+        public double EvaluateNeuralNetworkError(double desiredOutput, double output)
+        {
+            return (desiredOutput - output);
+        }
+
+        public void Learn(double[] neuron, double[] output)
+        {
+            for (int i = 0; i < neuron.Length; i++)
+            {
+                if (neuron[i] > 0) output[i] += LearningRate;
+            }
+        }
+
+        public void Train(int trails)
+        {
+            for (int i = 0; i < trails; i++)
+            {
+                double neuralNetResult = EvaluateNeuralNet(Neuron, Weight);
+
+                Console.WriteLine("Neural result: {0} Error: {1} Weight: {2}", neuralNetResult, EvaluateNeuralNetworkError(DesiredOutput, neuralNetResult), Weight);
+
+                Learn(Neuron, Weight);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ NeuralNet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NeuralNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] neurons = { 0, 0, 1, 0};
+            double[] weights = { 0, 0, 0, 0};
+
+            Console.Write("Times to trains the neural network: ");
+            int timesToTrain = Convert.ToInt32(Console.ReadLine());
+
+            Neurons neuron = new Neurons();
+
+            neuron.SetNeurons(neurons);
+            neuron.SetWeights(weights);
+
+            neuron.Train(timesToTrain);
+
+            Console.ReadLine();
+        }
+    }
+}
